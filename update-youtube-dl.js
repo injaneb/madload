3980409bd7df54aacbac7eb55419a7af17762504
Osama <osamaeletch@gmail.com>
2019-11-27 22:09:45 +0200

    stuff

--- update-youtube-dl.js
+++ update-youtube-dl.js
@@ -1,19 +1,11 @@
-// const path = require('path');
-// const { execFile } = require('child_process');
+const path = require('path');
+const { execFile } = require('child_process');
 
-// const file = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
+const file = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
 
-// execFile(file, ['-U'], (err, data) => {
-//   if (err) throw err;
-
-//   console.log(data);
-//   return data;
-// });
-
-const ytdl = require('youtube-dl');
-
-ytdl.exec('', ['-U'], {}, (err, output) => {
+execFile(file, ['-U'], (err, data) => {
   if (err) throw err;
 
-  console.log(output);
+  console.log(data);
+  return data;
 });
