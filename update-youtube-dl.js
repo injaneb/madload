4e098865291f3d5029bcaeeb642fd93ac2753f6d
Osama <osamaeletch@gmail.com>
2019-11-27 21:36:45 +0200

    initial commit

--- update-youtube-dl.js
+++ update-youtube-dl.js
@@ -1,11 +1,19 @@
-const path = require('path');
-const { execFile } = require('child_process');
+// const path = require('path');
+// const { execFile } = require('child_process');
 
-const file = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
+// const file = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
 
-execFile(file, ['-U'], (err, data) => {
+// execFile(file, ['-U'], (err, data) => {
+//   if (err) throw err;
+
+//   console.log(data);
+//   return data;
+// });
+
+const ytdl = require('youtube-dl');
+
+ytdl.exec('', ['-U'], {}, (err, output) => {
   if (err) throw err;
 
-  console.log(data);
-  return data;
+  console.log(output);
 });
