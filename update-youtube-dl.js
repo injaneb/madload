d0749fec06fa20d427d5cbce6e6742ac92e9dfcf
Osama <osamaeletch@gmail.com>
2019-11-29 06:12:53 +0200

    update the youtube-dl.exe

--- update-youtube-dl.js
+++ update-youtube-dl.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const { execFile } = require('child_process');
 
-const file = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
+const ytdlexe = path.join(__dirname, 'node_modules', 'youtube-dl', 'bin', 'youtube-dl.exe');
 
-execFile(file, ['-U'], (err, data) => {
+execFile(ytdlexe, ['-U'], (err, data) => {
   if (err) throw err;
 
   console.log(data);
