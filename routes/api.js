c6a7f9883521e622144cbc161c02ff9003b56ab0
Osama <osamaeletch@gmail.com>
2019-12-01 13:49:47 +0200

    add instagram to the list and fix some issues

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const express = require('express');
-const ytdl = require('youtube-dl');
-const logger = require('../lib/logger');
-const router = express.Router();
+const ytdl    = require('youtube-dl');
+const logger  = require('../lib/logger');
+const router  = express.Router();
 
 const supported = [
   'youtube.com',
@@ -21,13 +21,13 @@ router.get('/test', (req, res) => {
 });
 
 router.all('/', (req, res) => {
-  let url = req.body.url || req.query.url;
+  const url = req.body.url || req.query.url;
 
   if (!url) return res.json({ ok: false, err: 'url not found.' });
 
-  let urlMatch = url.match(/^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}[.]{0,1}/i);
+  let urlMatch = url.match(/^(https?:\/\/){0,1}(www\.){0,1}[\d-.a-z]+\.[a-z]{2,5}\.{0,1}/i);
   if (urlMatch && urlMatch.length > 0) {
-    let domainName = urlMatch[0].replace(/(http[s]?:\/\/)|(www\.)/ig, '');
+    let domainName = urlMatch[0].replace(/(https?:\/\/){0,1}(www\.){0,1}/i, '');
     if (!supported.includes(domainName)) {
       let errmsg = `${domainName} isn't supported.`;
 
@@ -49,107 +49,85 @@ router.all('/', (req, res) => {
     }
 
     const data = {
-      ok: true,
-      title: info.title,
-      url: info.webpage_url,
-      id: info.display_id,
+      ok:        true,
+      id:        info.display_id,
+      title:     info.title,
+      url:       info.webpage_url,
+      platform:  info.extractor,
       thumbnail: info.thumbnail,
-      platform: info.extractor,
+      duration:  info.duration && info.duration.includes(':')
+                  ? info.duration.split(':').map(t => t < 10 ? t = `0${~~t}` : ~~t).join(':')
+                  : `0:${info.duration < 10 ? `0${~~info.duration}` : ~~info.duration}` || null,
       author: {
         name: info.uploader,
-        url: info.uploader_url
+        url:  info.uploader_url || url.match(/^(https?:\/\/){0,1}(www\.){0,1}[\d-.a-z]+\.[a-z]{2,5}\/[\d-.a-z]+/i)[0]
       },
       formats: []
     };
 
-    // set duration
-    if (info.duration.indexOf(':') !== -1) {
-      data.duration = info.duration.split(':').map(t => t < 10 ? t = `0${~~t}` : ~~t).join(':');
-    } else {
-      data.duration = `0:${info.duration < 10 ? `0${~~info.duration}` : ~~info.duration}`;
-    }
+    if (data.platform !== 'soundcloud' && data.platform !== 'instagram') {
+      data.formats = info.formats.map(item => {
+        return {
+          url:        item.url,
+          ext:        item.ext,
+          acodec:     item.acodec,
+          vcodec:     item.vcodec,
+          format:     item.format,
+          formatId:   item.format_id,
+          formatNote: item.format_note
+        };
+      }).filter(item => {
+        // remove the id from the format
+        if (item.format.startsWith(item.formatId)) item.format = item.format.replace(`${item.formatId} - `, '').trim();
 
-    if (data.platform !== 'soundcloud') {
-      let formats = info.formats.filter(item => {
-        switch (data.platform) {
-          case 'youtube':
-            if (item.format_note.startsWith('DASH')) return false;
-            if ((item.ext === 'mp4' || item.ext === 'webm') && (item.format_note === 'tiny' || item.vcodec === 'none')) return false;
-          break;
+        if (data.platform === 'youtube') {
+          // remove DASH videos
+          if (item.formatNote.startsWith('DASH')) return false;
 
-          case 'twitter':
-            if (item.format_id.startsWith('hls')) return false;
-          break;
+          // remove audio only videos
+          if ((item.ext === 'mp4' || item.ext === 'webm') && (item.formatNote === 'tiny' || item.vcodec === 'none')) return false;
+        } else if (data.platform === 'twitter') {
+          // remove HLS videos
+          if (item.formatId.startsWith('hls')) return false;
         }
 
         return true;
+      }).sort((a, b) => {
+        if (data.platform === 'youtube') {
+          // sort by formatNote (1080p, 720p, etc..)
+          return parseInt(a.formatNote) > parseInt(b.formatNote) ? -1 : 1;
+        } else if (data.platform === 'twitter') {
+          // sort by resolution (720x1280, 360x640, etc..)
+          let ax = a.format.split('x');
+          let bx = b.format.split('x');
+          return (bx[1] - bx[0]) - (ax[1] - ax[0]);
+        }
+
+        return 0;
       });
 
-      if (formats.length > 1) {
-        formats.forEach((item, i) => {
-          // remove the id from the format
-          if (item.format.startsWith(item.format_id)) item.format = item.format.replace(`${item.format_id} - `, '').trim();
+      if (data.platform === 'youtube') {
+        // set thumbnail
+        data.thumbnail = data.thumbnail.replace('hqdefault', 'mqdefault');
 
-          data.formats[i] = {
-            url: item.url,
-            ext: item.ext,
-            acodec: item.acodec,
-            vcodec: item.vcodec,
-            format: item.format,
-            formatId: item.format_id,
-            formatNote: item.format_note
-          };
-        });
-      } else {
-        let item = formats[0];
-        data.formats[0] = {
-          url: item.url,
-          ext: item.ext,
-          acodec: item.acodec,
-          vcodec: item.vcodec,
-          format: item.format,
-          formatId: item.format_id,
-          formatNote: item.format_note
-        };
+        { // move best formats up
+          let formatMedium = data.formats.findIndex(item => item.formatId === '18');
+          let formatHigh   = data.formats.findIndex(item => item.formatId === '22');
+
+          if (formatMedium !== -1) data.formats.unshift(data.formats.splice(formatMedium, 1)[0]);
+          if (formatHigh   !== -1) data.formats.unshift(data.formats.splice(formatHigh, 1)[0]);
+        }
+
+        // move the audio up if its a music video
+        if (info.categories.includes('Music')) data.formats.unshift(data.formats.splice(data.formats.findIndex(item => item.ext === 'm4a'), 1)[0]);
       }
     } else {
       data.formats[0] = {
         url: info.url,
         ext: info.ext
       };
-    }
 
-    switch (data.platform) {
-      case 'youtube':
-        // set thumbnail
-        data.thumbnail = data.thumbnail.replace('hqdefault', 'mqdefault');
-
-        // sort by the formatNote
-        data.formats.sort((a, b) => parseInt(a.formatNote) > parseInt(b.formatNote) ? -1 : 1);
-
-        // move best formats up
-        let frmt18 = data.formats.findIndex(item => item.formatId === '18');
-        let frmt22 = data.formats.findIndex(item => item.formatId === '22');
-
-        if (frmt18 !== -1) data.formats.unshift(data.formats.splice(frmt18, 1)[0]);
-        if (frmt22 !== -1) data.formats.unshift(data.formats.splice(frmt22, 1)[0]);
-
-        // move the audio up if its a music video
-        if (info.categories.includes('Music')) data.formats.unshift(data.formats.splice(data.formats.findIndex(item => item.ext === 'm4a'), 1)[0]);
-       break;
-
-      case 'soundcloud':
-        data.thumbnail = data.thumbnail.replace('original', 'crop');
-      break;
-
-      case 'twitter':
-        // sort by width and height
-        data.formats.sort((a, b) => {
-          let ax = a.format.split('x');
-          let bx = b.format.split('x');
-          return (bx[1] - bx[0]) - (ax[1] - ax[0]);
-        });
-      break;
+      if (data.platform === 'soundcloud') data.thumbnail = data.thumbnail.replace('original', 'crop');
     }
 
     res.json(data);
